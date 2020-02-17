ff6ce69d93fbe87b5180051250ca5672d8d2da44
João Vitor <joaovitorcoelho13@gmail.com>
2020-02-17 13:28:59 -0300

    remove style

--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-
-import './config/ReactotronConfig';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import { View } from 'react-native';
 
 function App() {
-  return (
-    <>
-      <View style={styles.container}>
-        <Text>Hello, João</Text>
-      </View>
-    </>
-  );
+  return <View />;
 }
 
 export default App;
