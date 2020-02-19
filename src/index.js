73fcba9c004925bacfc80bf78e32dfe4eed4319a
João Vitor <joaovitorcoelho13@gmail.com>
2020-02-19 09:26:29 -0300

    statusbar color

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Routes from './routes';
 export default function App() {
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#fff" />
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <Routes />
     </>
   );
