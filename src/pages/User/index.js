479824b183a57e66a4eccee50aa758ebcaff307e
João Vitor <joaovitorcoelho13@gmail.com>
2020-02-20 10:41:21 -0300

    add page user

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,10 +1,75 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-// import { Container } from './styles';
+import api from '../../services/api';
 
-export default function User(props) {
-  const { user } = props.route.params;
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
-  return <Text> {user.name} </Text>;
+export default class User extends Component {
+  static propTypes = {
+    navigation: PropTypes.shape({
+      setOptions: PropTypes.func,
+    }).isRequired,
+    route: PropTypes.shape({
+      params: PropTypes.shape({
+        user: PropTypes.shape(),
+      }).isRequired,
+    }),
+  };
+
+  state = {
+    stars: [],
+    user: '',
+  };
+
+  async componentDidMount() {
+    const { navigation, route } = this.props;
+    const { user } = route.params;
+
+    navigation.setOptions({ title: user.name });
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({ stars: response.data, user });
+  }
+
+  render() {
+    const { stars, user } = this.state;
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
+    );
+  }
 }
